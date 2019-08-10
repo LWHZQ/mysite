507c3dbf41bcb94098f6088eb79d588ac46375af
leiwuhen92 <1162886013@qq.com>
2019-08-10 13:49:42 +0800

    2.6.3 编辑个人信息

--- account/urls.py
+++ account/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     url(r'^logout/', views.user_logout,name="user_logout"),#自定义退出
     url(r'^register/',views.register,name="user_rehister"),#自定义注册
 
-    url(r'^my-information/$',views.myself,name='my_infomation')
+    url(r'^my-information/$',views.myself,name='my_infomation'), #展示个人信息
+    url(r'^edit-my-information/$',views.myself_edit,name='edit_my_infomation')  #编辑个人信息
+
 
 
 
--- account/views.py
+++ account/views.py
@@ -1,12 +1,16 @@
 #coding:utf-8
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate,login
-from . forms import LoginForm,RegistrationForm,UserProfileForm
+from .forms import LoginForm,RegistrationForm,UserProfileForm,UserForm,UserInfoForm
+
 
 from django.contrib.auth.decorators import  login_required
 from  .models import UserInfo,UserProfile
 from django.contrib.auth.models import  User
+
+
+
 
 # Create your views here.
 
@@ -57,12 +61,50 @@ def register(request):
         return render(request,'account/register.html',{"form":user_form,"profile":userprofile_form})
 
 
-
+#展示个人信息
 @login_required(login_url='/account/login') #只有登录的用户才能看到自己的个人信息，没有登陆的用户转到登录界面
 def myself(request):
     user =User.objects.get(username=request.user.username)
     userprofile =UserProfile.objects.get(user=user)
     userinfo = UserInfo.objects.get(user=user)
     return render(request,"account/myself.html",{"user":user,"userinfo":userinfo,"userprofile":userprofile})
+
+
+#编辑个人信息
+@login_required(login_url='/account/login')
+def myself_edit(request):
+    user = User.objects.get(username=request.user.username)
+    userprofile =UserProfile.objects.get(user=user)
+    userinfo = UserInfo.objects.get(user=user)
+
+    if request.method =="POST":
+        user_form = UserForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        userinfo_form =UserInfoForm(request.POST)
+
+        if user_form.is_valid() * userprofile_form.is_valid() * userinfo_form.is_valid():
+            user_cd = user_form.cleaned_data
+            userprofile_cd = userprofile_form.cleaned_data
+            userinfo_cd = userinfo_form.cleaned_data
+            print(user_cd['email'])
+
+            user.email = user_cd['email']
+            userprofile.birth =userprofile_cd['birth']
+            userprofile.phone =userprofile_cd['phone']
+            userinfo.school =userinfo_cd['school']
+            userinfo.company =userinfo_cd['company']
+            userinfo.address =userinfo_cd['address']
+            userinfo.aboutme =userinfo_cd['aboutme']
+            user.save()
+            userprofile.save()
+            userinfo.save()
+
+            return HttpResponseRedirect('/account/my-information/')
+    else:
+        user_form =UserForm(instance=request.user)
+        userprofile_form =UserProfileForm(initial={"birth":userprofile.birth,"phone":userprofile.phone})
+        userinfo_form = UserInfoForm(initial={"school":userinfo.school,"company":userinfo.company,"address":userinfo.address,"aboutme":userinfo.aboutme})
+
+        return render(request,"account/myself_edit.html",{"user_form":user_form,"userprofile_form":userprofile_form,"userinfo_form":userinfo_form})
 
 
