5eef7425900bef850748b31c12f68261410ddf34
leiwuhen92 <1162886013@qq.com>
2019-08-11 21:38:11 +0800

    2.6.4上传 裁剪头像

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^login/', views.user_login,name="user_login"),#自定义登录
     url(r'^logout/', views.user_logout,name="user_logout"),#自定义退出
-    url(r'^register/',views.register,name="user_rehister"),#自定义注册
+    url(r'^register/',views.register,name="user_register"),#自定义注册
 
     url(r'^my-information/$',views.myself,name='my_information'), #展示个人信息
     url(r'^edit-my-information/$',views.myself_edit,name='edit_my_information'),  #编辑个人信息
