da858c5aea9ca4c4c1be608247f531c065ca6773
leiwuhen92 <1162886013@qq.com>
2019-09-08 11:52:38 +0800

    3.1.2编辑栏目 3.1.3删除栏目

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ArticleColumn
+
+class ArticleColumnAdmin(admin.ModelAdmin):
+    list_display=('column','user','created')
+    list_filter=('column',)
+
+admin.site.register(ArticleColumn,ArticleColumnAdmin)
+
