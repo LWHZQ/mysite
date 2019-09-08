ba72b425b2a98ea16aa7c8a642c888be35132bcd
leiwuhen92 <1162886013@qq.com>
2019-09-08 14:01:55 +0800

    2.5.1修改密码

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,17 @@
 #coding:utf-8
 from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^login/', views.user_login,name="user_login"),#自定义登录
     url(r'^logout/', views.user_logout,name="user_logout"),#自定义退出
     url(r'^register/',views.register,name="user_register"),#自定义注册
+
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name="account/password_change_form.html", success_url="/account/password-change-done/"), name='password_change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"), name='password_change_done'),
 
     url(r'^my-information/$',views.myself,name='my_information'), #展示个人信息
     url(r'^edit-my-information/$',views.myself_edit,name='edit_my_information'),  #编辑个人信息
--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE ='zh_hant'
+LANGUAGE_CODE ='zh_hans'
 
 TIME_ZONE = 'UTC'
 
